c25007faf2253d8228373662a7236caf236e3c90
AK Coding <naitik.kashyap5205@gmail.com>
2024-07-24 20:32:33 +0530

    Update and rename codes.js to code.js

---
+++ code.js
@@ -0,0 +1,27 @@
+let profile = {
+fullname : "Naitik Kashyap",
+age : 13,
+language : "HTML, CSS, JS, Python and Java",
+work : "Web Develeper",
+dream : "To became a professional Developer",
+}
+
+let a = 20
+let b = 10
+
+console.log("a = ", a "b = ", b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a ++ = ", a++);
+console.log("b++ = ", b++);
+console.log("++a = ", ++a);
+console.log("++b = ", ++b);
+console.log("a-- = ", a--);
+console.log("b-- = ", b--);
+console.log("--a = ", --a);
+console.log("--b = ", --b);
+console.log("a == b", a == b);
+console.log("a != b", a != b);
+console.log(profiile)
--- codes.js
+++
@@ -1,9 +0,0 @@
-let profile = {
-fullname : "Naitik Kashyap",
-age : 13,
-language : "HTML, CSS, JS, Python and Java",
-work : "Web Develeper",
-dream : "To became a professional Developer",
-}
-
-console.log(profiile)
